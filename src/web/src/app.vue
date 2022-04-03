bc31ae872b860637200a469a5ba596b0acf152a7
kade-d <kldentel@bsu.edu>
2022-04-02 23:25:46 -0400

    Add navigation buttons

--- src/web/src/app.vue
+++ src/web/src/app.vue
@@ -1,12 +1,33 @@
 <template>
   <the-shell>
-    <template #top> buttons </template>
+    <template #top>
+      <n-space class="nav" align="center" justify="center">
+        <router-link
+          v-for="r in topLevelRoutes"
+          :key="r.name"
+          :to="r.path"
+          custom
+          v-slot="{ href, route, navigate, isActive }"
+        >
+          <n-button text @click="navigate()">
+            {{ route.name }}
+          </n-button>
+        </router-link>
+      </n-space>
+    </template>
     <router-view></router-view>
   </the-shell>
 </template>
 
 <script setup lang="ts">
 import TheShell from "./components/the-shell.vue";
+import topLevelRoutes from "./router/top-level-routes";
+import { NButton, NSpace } from "naive-ui";
 </script>
 
-<style></style>
+<style scoped lang="scss">
+.nav {
+  height: 100%;
+  padding-left: 1rem;
+}
+</style>
